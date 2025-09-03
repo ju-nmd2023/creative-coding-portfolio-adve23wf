08a53d24819336c3580c995ab3cb0b069fb9d703
adve23wf <adve23wf@student.ju.se>
2025-09-03 20:19:01 +0300

    Shuffle squares

--- experiments/rectangles.js
+++ experiments/rectangles.js
@@ -3,23 +3,27 @@ function setup() {
   background(255, 255, 255);
 }
 
-const randomDisplacement = 15;
-const rotateMultiplier = 20;
 const size = 30;
 const amount = 5;
 const amount2 = 15;
 const gap = 20;
 
 function draw() {
-  //https://codepen.io/pixelkind/pen/bGOEmaV
-  let y = (height - size * amount2) / 2;
+  // isnpared by https://codepen.io/pixelkind/pen/bGOEmaV
+  rectMode(CENTER);
+  let y = (height - size * amount2) / 4;
 
   for (i = 0; i < amount2; i++) {
-    let x = (width - size * amount) / 2;
+    let x = (width - size * amount) / 4;
 
-    for (k = 0; k < amount; k++) {
+    for (j = 0; j < amount; j++) {
+      push();
+      let rot = random(0, size);
+      let rot2 = random(0, size);
+      translate(0 + rot, 0 + rot2);
       square(x, y, size);
       x += size;
+      pop();
     }
     y += size;
   }
